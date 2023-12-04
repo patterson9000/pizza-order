9e6c3a2a4a44ae44c87298819fa2ec568e7b9e08
bret patterson <patterson.bret@ymail.com>
2023-12-04 14:03:19 -0800

    fixed js

--- pizza.js
+++ pizza.js
@@ -22,16 +22,33 @@ function validatePizzaForm() {
   return true;
 }
 
-function calculatePizzaCost() {
-  // Calculate pizza cost logic here
+function displayResult(cost) {
+  const resultDiv = document.getElementById('result');
+  resultDiv.textContent = `Total Cost: $${cost.toFixed(2)}`;
+}
+
+function calculatePizzaCost(size, toppings, quantity) {
+  // Add your logic to calculate the cost based on size, toppings, and quantity
   // ...
+
+  // For demonstration purposes, let's assume a simple calculation
+  const baseCost = 10; // Replace with your actual base cost
+  const toppingsCost = toppings.length * 1.5; // Replace with your actual topping cost
+  const totalCost = (baseCost + toppingsCost) * quantity;
+
+  return totalCost;
 }
 
 const pizzaForm = document.getElementById('pizzaForm');
-pizzaForm.addEventListener('submit', function(event) {
+pizzaForm.addEventListener('submit', function (event) {
   event.preventDefault(); // Prevent form submission
 
   if (validatePizzaForm()) {
-    calculatePizzaCost();
+    const size = document.getElementById('size').value;
+    const toppings = document.querySelectorAll('input[name="toppings"]:checked');
+    const quantity = parseInt(document.getElementById('quantity').value);
+
+    const totalCost = calculatePizzaCost(size, toppings, quantity);
+    displayResult(totalCost);
   }
 });
