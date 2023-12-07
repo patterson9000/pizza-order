29b08b80e97da9f164e435f52f9909311f46e4cc
bret patterson <patterson.bret@ymail.com>
2023-12-07 15:58:06 -0800

    created contact form

---
+++ contact.js
@@ -0,0 +1,8 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Add your validation logic here if needed
+
+    // Display an alert for demonstration purposes
+    alert('Form submitted successfully!');
+});
